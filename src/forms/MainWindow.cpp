bf636a3f56662ac4215ec24b1ae47ccc7f81990b
Felix Mellmann <flix@benfm.de>
2020-09-11 14:16:34 +0200

    Aligned device selection dialog.

--- src/forms/MainWindow.cpp
+++ src/forms/MainWindow.cpp
@@ -31,7 +31,6 @@ void MainWindow::deviceDiscoveryFinished(void)
     m_ui->btnConnect->setEnabled(true);
     const auto& discoveredDevices = m_bluetoothDiscoveryController.discoveredDevices();
 
-    qInfo() << "MainWindow::deviceDiscoveryFinished(" << discoveredDevices.count() << ")";
     if (discoveredDevices.count() == 1)
     {
         m_bluetoothController.connectToDevice(discoveredDevices.first());
